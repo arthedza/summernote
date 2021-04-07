45a6f50ebb8bbcffa19bb52b1c2dbdfc2303df0c
Artur Zahorskyi <arthedza@gmail.com>
2021-04-07 18:10:21 +0300

    update build

--- dist/lang/summernote-pt-PT.js
+++ dist/lang/summernote-pt-PT.js
@@ -7,7 +7,7 @@
  * Copyright 2013- Alan Hong and other contributors
  * Summernote may be freely distributed under the MIT license.
  *
- * Date: 2021-04-07T14:23Z
+ * Date: 2021-04-07T15:09Z
  *
  */
 (function webpackUniversalModuleDefinition(root, factory) {
